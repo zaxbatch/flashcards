888f3005855be8838538740b94bbe144765c27d3
Dotcom <sneakjunkmail@gmail.com>
2018-08-03 21:03:34 -0400

    first commit

---
+++ app.js
@@ -0,0 +1,39 @@
+const express = require('express');
+
+const bodyParser = require('body-parser');
+
+const cookieParser = require('cookie-parser');
+
+const app = express();
+
+app.use(bodyParser.urlencoded({ extended:false }));
+app.use(cookieParser());
+app.use('/static',express.static('public'));
+app.use(( req, res, next) => { 
+	console.log("World");
+	next();
+});
+
+app.set('view engine', 'pug');
+
+const mainRoutes = require('./routes');
+const cardRoutes = require('./routes/cards');
+
+app.use(mainRoutes);
+app.use('/cards', cardRoutes);
+
+app.use((req, res, next) => {
+	const err = new Error('Not Found');
+	err.status = 404;
+	next(err);
+});
+
+app.use((err, req, res, next) => {
+	res.locals.error = err;
+	res.status(err.status);
+	res.render('error');
+});
+
+app.listen(3000, () => {
+	console.log('app running on loacal host 3000')
+}); 
